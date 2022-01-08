0bc56bfafb377a4bcf16d196e077677ea9520244
matthewlese <matthewjlese@gmail.com>
2022-01-07 17:47:59 -0800

    connected to mongo

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose');
+
 const app = express();
+const db = require('./config/keys').mongoURI;
+
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello World 2"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
