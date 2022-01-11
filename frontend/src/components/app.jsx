e6001786f124054d883575f1c507a2f7be51eecf
matthewlese <matthewjlese@gmail.com>
2022-01-11 12:37:49 -0800

    renders react app

---
+++ frontend/src/components/app.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+const App = () => {
+  return(
+  <div id='app' className='h-screen bg-yellow-700 bg-opacity-40'>
+    <Switch>
+      {/* <Route exact path='/' component={HeaderContainer}/>
+      <Route exact path='/recipes/new' component={null}/>
+      <Route exact path='/recipes/:recipeId' component={HeaderContainer}/> */}
+    </Switch>
+    <div className='relative pb-10'>
+      <Switch>
+        {/* <Route exact path='/' component={RecipesIndexContainer}/>
+        <ProtectedRoute exact path='/recipes/new' component={RecipeFormContainer}/>
+        <ProtectedRoute exact path='/recipes/:recipeId' component={RecipeShowContainer}/>
+        <ProtectedRoute exact path='/users/:userId' component={UserShowContainer}/> */}
+      </Switch>
+    </div>
+    <footer className='bg-yellow-900 opacity-90 absolute bottom-0 h-20 w-screen'>
+      <div className='flex justify-between max-w-6xl mx-auto px-4'>
+        <div className='flex-shrink-0 w-64 mx-auto text-center '>
+          <h3 className='text-white text-opacity-70 italic font-light mt-3'>More from Matthew Lese</h3>
+          <div className='inline-flex mx-auto mt-2'>
+            <a href="https://github.com/matthewlese" className='mr-3'>
+              <div className='text-white text-opacity-70 font-light'>GitHub</div>
+            </a>
+            <a href="https://www.linkedin.com/in/matthewlese/" className='linkedin-link'>
+              <div className='text-white text-opacity-70 font-light'>LinkedIn</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+    {/* <Modal /> */}
+  </div>
+  )
+}
+
+export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Root from './Root';
+import configureStore from './store/store'
+
+let store
+if (window.currentUser) {
+  const currentUser = window.currentUser
+  const preloadedState = {
+    session: { currentUserId: currentUser.id },
+    entities: {
+      users: { [currentUser.id]: currentUser }
+    }
+  }
+  store = configureStore(preloadedState)
+  delete window.currentUser
+} else {
+  store = configureStore()
+}
+
+window.store = store
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
