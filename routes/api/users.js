2e8ffcc8c904957853592ccd1985ef2c800d7430
matthewlese <matthewjlese@gmail.com>
2022-01-13 15:19:13 -0800

    user deletion working in postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -86,4 +86,30 @@ router.post('/signin', (req, res) => {
     })
 })
 
+router.delete('/delete', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ username: 'This user does not exist' })
+      }
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            User.deleteOne({ username }, err => {
+              if (err) { 
+                return res.status(400).json('Something went wrong.')
+              } else {
+                return res.json(`${username} deleted.`)
+              }
+            })
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        })
+    })
+})
+
 module.exports = router;
