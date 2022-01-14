8b3af200d5c54ed4f486d5888caca1ee1b8b9f6f
matthewlese <matthewjlese@gmail.com>
2022-01-13 16:59:58 -0800

    combo index and show get route working

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -9,27 +9,43 @@ const passport = require('passport');
 const Recipe = require("../../models/Recipe");
 const validateRecipeInput = require('../../validations/recipe');
 
-router.post('/create', (req, res) => {
-  const { errors, isValid } = validateRecipeInput(req.body)
+router.post('/create', passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    const { errors, isValid } = validateRecipeInput(req.body)
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-  let { title, authorId } = req.body
-  Recipe.findOne({
-    authorId: authorId, // filter by the author (current user)
-    title: title        // filter by title
-  }).then(recipe => {
-    if (recipe) {
-      errors.title = 'You already have a recipe with this title.'
-      return res.status(400).json(errors) // bad request
-    } else {
-      const newRecipe = new Recipe(req.body)
-      newRecipe.save()
-        .then(recipe => res.json(recipe))
-        .catch(err => res.status(400).json(err));
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
-  })
+    let { title, authorId } = req.body
+    Recipe.findOne({
+      authorId: authorId, // filter by the author (current user)
+      title: title        // filter by title
+    }).then(recipe => {
+      if (recipe) {
+        errors.title = 'You already have a recipe with this title.'
+        return res.status(400).json(errors) // bad request
+      } else {
+        const newRecipe = new Recipe(req.body)
+        newRecipe.save()
+          .then(recipe => res.json(recipe))
+          .catch(err => res.status(400).json(err));
+      }
+    })
 })
+
+router.get('/', (req, res) => {
+  if (!req.body.recipeId) { // index
+    Recipe.find()
+      .then(recipes => res.json(recipes))
+      .catch(err => res.status(404).json({ noRecipesFound: 'No recipes found.'}))
+  } else { // show
+    Recipe.findById(req.body.recipeId)
+      .then(recipe => res.json(recipe))
+      .catch(err => res.status(404).json({ noRecipeFound: 'No recipe found.'}))
+  }
+})
+
+// router.get('/', (req, res) => {
+// })
 
 module.exports = router
