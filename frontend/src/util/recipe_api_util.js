0f53376d5b500e62f053c116aa7119de0294669b
matthewlese <matthewjlese@gmail.com>
2022-01-13 17:11:55 -0800

    recipe api util done for create and read

--- frontend/src/util/recipe_api_util.js
+++ frontend/src/util/recipe_api_util.js
@@ -1,5 +1,16 @@
 import axios from 'axios'
 
+export const showAllRecipes = async () => {
+  const response = await axios.get('/api/recipes')
+  return response.data;
+}
+
 export const showRecipe = async (recipeId) => {
   const response = await axios.get(`/api/recipes${recipeId}`)
+  return response.data;
 }
+
+export const createRecipe = async (recipe) => {
+  const response = await axios.post("/api/recipes/create", recipe);
+  return response.data;
+};
