20f4e69eba10012f273aa98e00b48ad0feb9b0e8
matthewlese <matthewjlese@gmail.com>
2022-01-13 17:23:49 -0800

    recipe actions set

---
+++ frontend/src/actions/recipe_actions.js
@@ -0,0 +1,35 @@
+import * as RecipeAPIUtil from '../util/recipe_api_util'
+
+export const RECEIVE_ALL_RECIPES = 'RECEIVE_ALL_RECIPES'
+export const RECEIVE_RECIPE = 'RECEIVE_RECIPE'
+export const RECEIVE_RECIPE_ERRORS = 'RECEIVE_RECIPE_ERRORS'
+
+export const receiveAllRecipes = recipes => ({
+  type: RECEIVE_ALL_RECIPES,
+  recipes
+})
+
+export const receiveRecipe = recipe => ({
+  type: RECEIVE_RECIPE,
+  recipe
+})
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_RECIPE_ERRORS,
+  errors
+})
+
+export const getRecipe = recipeId => dispatch =>
+  RecipeAPIUtil.showRecipe(recipeId)
+    .then(recipe => dispatch(receiveRecipe(recipe)))
+    .catch(err => dispatch(receiveErrors(err)))
+
+export const getAllRecipes = () => dispatch =>
+  RecipeAPIUtil.showAllRecipes()
+    .then(recipes => dispatch(receiveAllRecipes(recipes)))
+    .catch(err => dispatch(receiveErrors(err)))
+    
+export const createRecipe = recipe => dispatch =>
+  RecipeAPIUtil.createRecipe(recipe)
+    .then(recipe => dispatch(receiveRecipe(recipe)))
+    .catch(err => dispatch(receiveErrors(err)))
