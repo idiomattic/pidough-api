dc1bcbb193683a61da70d4b7ef1d34742d50d319
matthewlese <matthewjlese@gmail.com>
2022-01-13 17:32:31 -0800

    entities and errors reducers include recipes

---
+++ frontend/src/reducers/recipes_reducer.js
@@ -0,0 +1,21 @@
+import {
+  RECEIVE_ALL_RECIPES,
+  RECEIVE_RECIPE
+} from "../actions/recipe_actions";
+
+const recipesReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const nextState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RECEIVE_RECIPE:
+      nextState[action.recipe._id] = action.recipe;
+      return nextState;
+    case RECEIVE_ALL_RECIPES:
+      return action.recipes;
+    default:
+      return state;
+  }
+};
+
+export default recipesReducer;
