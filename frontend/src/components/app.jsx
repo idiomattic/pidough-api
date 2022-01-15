02539711ed24b7071182a8e7c20a51b3355ff12e
matthewlese <matthewjlese@gmail.com>
2022-01-14 20:47:37 -0800

    footer sticks to bottom and app takes full height

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -9,14 +9,17 @@ import CreateRecipeFormContainer from '../components/recipes/create_recipe_form_
 
 const App = () => {
   return(
-  <div id='app' className='h-screen bg-yellow-700 bg-opacity-40'>
+  <div id='app' className='min-h-screen bg-yellow-700 bg-opacity-40'>
     <HeaderContainer />
     {/* <Switch> */}
       {/* <Route exact path='/' component={HeaderContainer}/>
       <Route exact path='/recipes/new' component={null}/>
       <Route exact path='/recipes/:recipeId' component={HeaderContainer}/> */}
     {/* </Switch> */}
-    <div className='relative pb-10'>
+    <div className='flex flex-col min-h-[95vh]'>
+
+    
+    <div className='relative pb-10 overflow-hidden'>
       <Switch>
         <Route exact path='/' component={null}/>
         <ProtectedRoute exact path='/recipes/create' component={CreateRecipeFormContainer}/>
@@ -25,7 +28,7 @@ const App = () => {
         <ProtectedRoute exact path='/users/:userId' component={UserShowContainer}/> */}
       </Switch>
     </div>
-    <footer className='bg-yellow-900 opacity-90 absolute bottom-0 h-20 w-screen'>
+    <footer className='bg-yellow-900 relative bottom-0 h-20 w-screen'>
       <div className='flex justify-between max-w-6xl mx-auto px-4'>
         <div className='flex-shrink-0 w-64 mx-auto text-center '>
           <h3 className='text-white text-opacity-70 italic font-light mt-3'>More from Matthew Lese</h3>
@@ -40,6 +43,7 @@ const App = () => {
         </div>
       </div>
     </footer>
+    </div>
     <Modal />
   </div>
   )
