1520e4402f800de22dfd5b67f6847377702fa6e9
matthewlese <matthewjlese@gmail.com>
2022-01-14 21:13:26 -0800

    form styling done for now

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -11,38 +11,28 @@ const App = () => {
   return(
   <div id='app' className='min-h-screen bg-yellow-700 bg-opacity-40'>
     <HeaderContainer />
-    {/* <Switch> */}
-      {/* <Route exact path='/' component={HeaderContainer}/>
-      <Route exact path='/recipes/new' component={null}/>
-      <Route exact path='/recipes/:recipeId' component={HeaderContainer}/> */}
-    {/* </Switch> */}
-    <div className='flex flex-col min-h-[95vh]'>
-
-    
-    <div className='relative pb-10 overflow-hidden'>
-      <Switch>
-        <Route exact path='/' component={null}/>
-        <ProtectedRoute exact path='/recipes/create' component={CreateRecipeFormContainer}/>
-        {/* <Route exact path='/' component={RecipesIndexContainer}/> */}
-        {/* <ProtectedRoute exact path='/recipes/:recipeId' component={RecipeShowContainer}/>
-        <ProtectedRoute exact path='/users/:userId' component={UserShowContainer}/> */}
-      </Switch>
-    </div>
-    <footer className='bg-yellow-900 relative bottom-0 h-20 w-screen'>
-      <div className='flex justify-between max-w-6xl mx-auto px-4'>
-        <div className='flex-shrink-0 w-64 mx-auto text-center '>
-          <h3 className='text-white text-opacity-70 italic font-light mt-3'>More from Matthew Lese</h3>
-          <div className='inline-flex mx-auto mt-2'>
-            <a href="https://github.com/matthewlese" className='mr-3'>
-              <div className='text-white text-opacity-70 font-light'>GitHub</div>
-            </a>
-            <a href="https://www.linkedin.com/in/matthewlese/" className='linkedin-link'>
-              <div className='text-white text-opacity-70 font-light'>LinkedIn</div>
-            </a>
+    <div className='flex flex-col justify-between min-h-[95vh]'>
+      <div className='relative pb-6 overflow-hidden'>
+        <Switch>
+          <Route exact path='/' component={null}/>
+          <ProtectedRoute exact path='/recipes/create' component={CreateRecipeFormContainer}/>
+        </Switch>
+      </div>
+      <footer className='bg-yellow-900 relative bottom-0 h-20 w-screen'>
+        <div className='flex justify-between max-w-6xl mx-auto px-4'>
+          <div className='flex-shrink-0 w-64 mx-auto text-center '>
+            <h3 className='text-white text-opacity-70 italic font-light mt-3'>More from Matthew Lese</h3>
+            <div className='inline-flex mx-auto mt-2'>
+              <a href="https://github.com/matthewlese" className='mr-3'>
+                <div className='text-white text-opacity-70 font-light cursor-pointer hover:italic'>GitHub</div>
+              </a>
+              <a href="https://www.linkedin.com/in/matthewlese/" className='linkedin-link'>
+                <div className='text-white text-opacity-70 font-light cursor-pointer hover:italic'>LinkedIn</div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
     </div>
     <Modal />
   </div>
