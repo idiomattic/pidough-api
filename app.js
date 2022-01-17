58cc4329d8e9c17ffd65deeab10b722c3a5b6282
matthewlese <matthewjlese@gmail.com>
2022-01-17 13:04:27 -0800

    redirect

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
-    if (req.headers.host === 'pidough.herokuapp.com' || 'pidough.app') {
+    if (req.headers.host === 'pidough.herokuapp.com') {
       return res.redirect(301, 'http://www.pidough.xyz');
     }
     if (req.headers['x-forwarded-proto'] !== 'https') {
