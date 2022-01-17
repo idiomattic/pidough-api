0bf4bf7b6dc8505e5ac571130803c85f477cbe8d
matthewlese <matthewjlese@gmail.com>
2022-01-16 17:10:39 -0800

    configures keys for production

--- app.js
+++ app.js
@@ -12,23 +12,6 @@ const recipes = require('./routes/api/recipes')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use((req, res, next) => {
-//     if (req.headers.host === 'teacher-pet.herokuapp.com') {
-//       return res.redirect(301, 'https://www.teacherspet.io');
-//     }
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect('https://' + req.headers.host + req.url);
-//     } else {
-//       return next();
-//     }
-//   })
-//   app.use(express.static('frontend/build'));
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   })
-// }
-
 app.get("/", (req, res) => res.send("Hello World 2"));
 app.use("/api/users", users);
 app.use('/api/recipes', recipes)
